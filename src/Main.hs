18b2e7cf96ea9d040d9399d2a522ee74f2bffff3
TomasFeeney <tomas.feeney10@gmail.com>
2018-03-06 12:46:53 +0000

    attempt

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,21 @@
 module Main where
 
+--data Expr = Lit Integer | Add Expr Expr | Mul Expr Expr deriving (Eq, Show)
+
+class Eval a where
+    eval :: a -> Integer
+
+
+data Lit = Lit Integer
+instance Eval Lit where 
+    eval (Lit n) = n
+
+
+--eval :: Expr -> Integer
+--eval (Lit x) = x
+--eval (Add x y) = eval(x) + eval(y)
+--eval (Mul x y) = eval(x) * eval(y)
+
 main :: IO ()
 main = putStrLn "Hello, world!"
 
